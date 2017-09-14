57512b6913c55f792f58d2a3a33c2ce7a73064b0
MasseyIsaako <lasiisaako@gmail.com>
2017-09-15 10:00:13 +1200

    Added the html structure + grunt

---
+++ gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		cssmin:{
+			target:{
+				files:[{
+					expand:true,
+					cwd: "public/css/",
+					src: ["*.css", "!*.min.css"],
+					dest: "public/css/",
+					ext: ".min.css"
+				}]
+			}
+		},
+		sass:{
+			dist:{
+				files:{
+					"public/css/style.css":"public/sass/style.scss"
+				}
+			}
+		},
+		watch:{
+			cssmin:{
+				files:["public/css/style.css"],
+				tasks:["cssmin"]
+			},
+			sass:{
+				files:["public/sass/style.scss"],
+				tasks:["sass"]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-sass");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+
+	grunt.registerTask("default", ["cssmin"]);
+	grunt.registerTask("minjs", ["uglify"]);
+	grunt.registerTask("hint", ["jshint"]);
+	grunt.registerTask("scss", ["sass"]);
+	grunt.registerTask("w", ["watch"]);
+};
